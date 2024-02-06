c72ebee259482cba96cd839ef3d7110772576a89
Qendrim Behrami <behrami@in.tum.de>
2024-02-06 20:31:51 +0100

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+/**
+ * Remove elements from arr that are part of values
+ * @param {Array} arr the original array
+ * @param {...Number} values that shall be removed
+ */
+const removeFromArray = function (arr, ...values) {
+  let result = arr;
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i];
+    result = result.filter((element) => {
+      return element !== value;
+    });
+  }
+  return result;
 };
 
 // Do not edit below this line
