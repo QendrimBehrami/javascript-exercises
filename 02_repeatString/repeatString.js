a4135a4288005653b926d05c407ac903847e4768
Qendrim Behrami <behrami@in.tum.de>
2024-02-06 20:05:36 +0100

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+/**
+ *
+ * @param {string} str
+ * @param {Number} n
+ */
+const repeatString = function (str, n) {
+  if (n < 0) {
+    return "ERROR";
+  } else if (!str || n == 0) {
+    return "";
+  } else if (n === 1) {
+    return str;
+  } else return str + repeatString(str, n - 1);
 };
 
 // Do not edit below this line
