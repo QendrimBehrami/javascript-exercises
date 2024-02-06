1c003155451df9007f5754f5663eedd2f4059ce7
Qendrim Behrami <behrami@in.tum.de>
2024-02-06 20:47:41 +0100

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+/**
+ * Compute the sum of all numbers between a and b
+ * @param {Number} a
+ * @param {Number} b
+ */
+const sumAll = function (a, b) {
+  // Only allow positive numbers
+  if (typeof a != "number" || a < 0 || typeof b != "number" || b < 0) {
+    return "ERROR";
+  }
+  let lower = Math.min(a, b);
+  let upper = Math.max(a, b);
 
+  // Apply Gauss sum for ranges
+  return 0.5 * ((lower + upper) * (upper - lower + 1));
 };
 
 // Do not edit below this line
