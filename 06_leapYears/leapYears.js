eddcd3522dbcd0c205b6a9afaa85916bd828a848
Qendrim Behrami <behrami@in.tum.de>
2024-02-06 20:55:56 +0100

    Solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+/** Determine if the year was a leap year
+ * Can probably be coded better, but I find it very readable
+ * @param {Number} year
+ * @return {Boolean}
+ */
+const leapYears = function (year) {
+  // Not leap year if year is not multiple of 4 (e.g. 2003)
+  if (year % 4 != 0) {
+    return false;
+  }
+  // Also not leap year if year is multiple of 100, but not 400
+  if (year % 100 == 0 && year % 400 != 0) {
+    return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
