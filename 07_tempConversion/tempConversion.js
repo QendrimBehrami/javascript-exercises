2cf719c964e3d0d6735a6d73463bd37d5dc727e8
Qendrim Behrami <behrami@in.tum.de>
2024-02-06 21:04:00 +0100

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
+/**
+ *
+ * @param {Number} degreesFahrenheit
+ */
+const convertToCelsius = function (degreesFahrenheit) {
+  return (((degreesFahrenheit - 32) * 5) / 9).toFixed(1) * 1.0;
 };
 
-const convertToFahrenheit = function() {
+/**
+ *
+ * @param {Number} degreesCelsius
+ */
+const convertToFahrenheit = function (degreesCelsius) {
+  return ((degreesCelsius * 9) / 5 + 32).toFixed(1) * 1.0;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
