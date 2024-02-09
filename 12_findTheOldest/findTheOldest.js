d15159f59c4e5cb31a3574d7f1b1ed0195d5906a
Qendrim Behrami <behrami@in.tum.de>
2024-02-09 22:42:37 +0100

    Solve Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+/**
+ *
+ * @param {Array} people
+ */
+const findTheOldest = function (people) {
+  let oldest = null;
+  for (key in people) {
+    let currentPerson = people[key];
+    if (calculateAge(currentPerson) > calculateAge(oldest)) {
+      oldest = currentPerson;
+    }
+  }
 
+  return oldest;
 };
+
+function calculateAge(person) {
+  if (person == null) {
+    return -1; //xdd
+  }
+  let currentYear = new Date().getFullYear();
+  let deathYear =
+    person.yearOfDeath === undefined ? currentYear : person.yearOfDeath;
+  return deathYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
