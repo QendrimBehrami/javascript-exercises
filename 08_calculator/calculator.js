9a5f6a0e795a01d7c69901472b1131998510a407
Qendrim Behrami <behrami@in.tum.de>
2024-02-09 20:44:22 +0100

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function (a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  return a - b;
 };
 
-const sum = function() {
-	
+/**
+ *
+ * @param {Array} numbers
+ */
+const sum = function (numbers) {
+  let result = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    result += parseInt(numbers[i]);
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let result = 1;
+  for (let i = 0; i < numbers.length; i++) {
+    result *= parseInt(numbers[i]);
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  base = parseInt(base);
+  exponent = parseInt(exponent);
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1;
+  for (i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +51,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
