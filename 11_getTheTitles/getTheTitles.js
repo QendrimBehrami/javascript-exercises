b23ce01892413fe513d259973d5f50dfb7059b7b
Qendrim Behrami <behrami@in.tum.de>
2024-02-09 22:27:51 +0100

    Solve Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (library) {
+  let result = new Array();
+  for (key in library) {
+    let book = library[key];
+    result.push(book.title);
+  }
+  return result;
 };
 
 // Do not edit below this line
